56ef103e55e8d0ceee63c177eeea8e9ace484818
Yu Lu <ylu60@stuy.edu>
2025-12-04 10:32:18 -0500

    push

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+#Sean Takahashi, James Sun, Kevin Liu, Yu Lu
+#Ninja Cat
+
+# Imports
+from flask import Flask, render_template, request, flash, url_for, redirect, session
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+import entries, auth
+
+# Initialize databases
+
+app = Flask(__name__)
+app.secret_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
+
+@app.context_processor
+def user_context(): # persistent info made avalible for all html templates
+    return {
+    }
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template("index.html")
+
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
