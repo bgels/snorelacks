74486a197c46ea5ea71c4587d99fe1991647a57a
Ricky Lin <rlin61@stuy.edu>
2025-12-04 10:35:21 -0500

    draft

--- app/__init__.py
+++ app/__init__.py
@@ -16,8 +16,17 @@ app.secret_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
 def index():
     return render_template("index.html")
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    return render_template("index.html")
 
+@app.route("/homepage", methods=['GET', 'POST'])
+def home():
+    return render_template("index.html")
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.debug = True
