5748970acad62d664cde13df6aacf66a7fb345c7
mabdullah32 <mabdullah32@github.com>
2025-12-08 11:01:32 -0500

    weather parsing

--- app/__init__.py
+++ app/__init__.py
@@ -19,20 +19,28 @@ import pprint
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    
-    countries = urllib.request.urlopen(f"https://restcountries.com/v3.1/name/pakistan?fields=capital,currencies,languages,name,population,timezones,flag")
+    countries = urllib.request.urlopen(f"https://restcountries.com/v3.1/name/pakistan?fields=capital,currencies,languages,name,population,timezones,flag,latlng")
+    countries_info = json.load(countries)
 
-    a = json.load(countries)
+    weather_key = open("keys/key_api1.txt")
+    weather = urllib.request.urlopen(f"https://api.openweathermap.org/data/2.5/forecast?lat={countries_info[0]['latlng'][0]}&lon={countries_info[0]['latlng'][1]}&units=metric&&appid={weather_key.read()}")
+    weather_info = json.load(weather)
+ 
+
+
     
        
     
-    pprint.pprint(a)
+    pprint.pprint(countries_info)
+    pprint.pprint(weather_info)
     #capital, currency, languages, name, population, timezone, flag for each country,add more later
 #name, capital, currency, population, timezone, languages
     
     
     
-    return render_template("index.html", name = a[0]["name"]["common"], capital = a[0]["capital"][0], currency = a[0]["currencies"], population = a[0]["population"], timezone = a[0]["timezones"][0])
+    return render_template("index.html", 
+        name = countries_info[0]["name"]["common"], capital = countries_info[0]["capital"][0], currency = countries_info[0]["currencies"], population = countries_info[0]["population"], timezone = countries_info[0]["timezones"][0]
+    )
 
     
 
@@ -51,4 +59,5 @@ def index():
 if __name__ == "__main__":
     app.debug = True
     app.run()
+
 
