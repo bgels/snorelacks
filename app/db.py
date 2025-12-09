10d26b72e8d362643c9eb81158681e1b2aab7b06
Ricky Lin <rlin61@stuy.edu>
2025-12-09 10:31:52 -0500

    fixed bindings

--- app/db.py
+++ app/db.py
@@ -10,15 +10,15 @@ def add_user(username, password, country, currency):
     DB_NAME = "Data/database.db"
     DB = sqlite3.connect(DB_NAME)
     DB_CURSOR = DB.cursor()
-    DB_CURSOR.execute("SELECT COUNT(*) FROM Users WHERE username = ?", username)
-    cursorfetch = cursor.fetchone()[0]
+    DB_CURSOR.execute("SELECT COUNT(*) FROM Users WHERE username = (?)", (username,))
+    cursorfetch = DB_CURSOR.fetchone()[0]
     if cursorfetch == 1:
-        db.commit()
-        db.close()
+        DB.commit()
+        DB.close()
         return False
-    DB_CURSOR.execute("INSERT INTO Users VALUES(?, ?, ?, ?)", username, password, country, currency)
-    db.commit()
-    db.close()
+    DB_CURSOR.execute("INSERT INTO Users VALUES(?, ?, ?, ?)", (username, password, country, currency))
+    DB.commit()
+    DB.close()
     return True
 
 def get_user(username):
