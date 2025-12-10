ef9acd2f9bdd2e95b0ee740b5aa06b25c83ec26f
Mustafa A <abdullahmustafa050@gmail.com>
2025-12-10 00:43:27 -0500

    added api initial setup

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,50 @@ from flask import Flask, render_template, request, flash, url_for, redirect, ses
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 import db
+import json
+from urllib.request import Request, urlopen
+import pprint
 
 # Initialize databases
 
 app = Flask(__name__)
 app.secret_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
 
+def api_content():
+    countries = urlopen(f"https://restcountries.com/v3.1/name/pakistan?fields=capital,currencies,languages,name,population,timezones,flag,latlng,capitalInfo")
+    countries_info = json.load(countries)
+
+    weather_key = open("keys/key_api1.txt")
+    weather = urlopen(f"https://api.openweathermap.org/data/2.5/forecast?lat={countries_info[0]['latlng'][0]}&lon={countries_info[0]['latlng'][1]}&units=metric&&appid={weather_key.read()}")
+    weather_info = json.load(weather)
+    #https://en.wikipedia.org/w/api.php?action=parse&page=Pakistan&prop=sections&format=json
+    wikipedia_req = Request(
+        url=f"https://en.wikipedia.org/w/api.php?action=parse&page=Pakistan&section=41&prop=text&format=json&formatversion=2",
+        headers={'User-Agent': 'Mozilla/5.0'}
+    ) 
+    wikipedia = urlopen(wikipedia_req, timeout=10)
+    wikipedia_info = json.load(wikipedia)
+    wikipedia_html = wikipedia_info['parse']['text']
+    # pprint.pprint(wikipedia_html)
+    culture_section_index = None
+    
+    places_key = open("keys/key_api2.txt")
+    places = urlopen(f"https://api.geoapify.com/v2/places?categories=tourism.attraction&filter=circle:{countries_info[0]['capitalInfo']['latlng'][0]},{countries_info[0]['capitalInfo']['latlng'][1]},5000&limit=20&apiKey={places_key.read()}")
+    places_info = json.load(places)
+
+    exchange_key = open("keys/key_api3.txt")
+    exchange_rate = urlopen(f"https://api.exchangerateapi.net/v1/latest?base=USD&currencies=PKR&apikey={exchange_key.read()}")
+    exchange_info = json.load(exchange_rate)
+    
+    # pprint.pprint(wikipedia_info)  
+    #pprint.pprint(countries_info)
+    #pprint.pprint(weather_info)
+    #pprint.pprint(exchange_info)
+    #capital, currency, languages, name, population, timezone, flag for each country,add more later
+#name, capital, currency, population, timezone, languages
+    # return render_template("index.html", 
+    #     name = countries_info[0]["name"]["common"], capital = countries_info[0]["capital"][0], currency = countries_info[0]["currencies"], population = countries_info[0]["population"], timezone = countries_info[0]["timezones"][0]
+    # )
 
 @app.context_processor
 def user_context(): # persistent info made avalible for all html templates
