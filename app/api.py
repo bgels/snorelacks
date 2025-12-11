36a8586e8c47f12e5343e06b0c7cdd64dac7dee7
mabdullah32 <mabdullah32@github.com>
2025-12-11 11:24:06 -0500

    fixed api parsing function

--- app/api.py
+++ app/api.py
@@ -10,12 +10,17 @@ from bs4 import BeautifulSoup
 
 
 def extract_country_data(country):
-    countries = urlopen(f"https://restcountries.com/v3.1/name/{country}?fields=capital,currencies,languages,name,population,timezones,flag,latlng")
+    country_data = {}
+    user_currency = "USD"
+
+    countries = urlopen(f"https://restcountries.com/v3.1/name/{country}?fields=capital,currencies,languages,name,population,timezones,flag,latlng,capitalInfo")
     countries_info = json.load(countries)
+    country_data["country"] = countries_info
 
     weather_key = open("keys/key_api1.txt")
     weather = urlopen(f"https://api.openweathermap.org/data/2.5/forecast?lat={countries_info[0]['latlng'][0]}&lon={countries_info[0]['latlng'][1]}&units=metric&&appid={weather_key.read()}")
     weather_info = json.load(weather)
+    country_data["weather"] = weather_info
 
     wiki_summary_req = Request(
          url=f"https://en.wikipedia.org/api/rest_v1/page/summary/{country}",
@@ -23,23 +28,26 @@ def extract_country_data(country):
     )
     wiki_summary = urlopen(wiki_summary_req, timeout=10)
     wiki_summary_info = json.load(wiki_summary)
+    country_data["summary"] = wiki_summary_info
 
     places_key = open("keys/key_api2.txt")
     places = urlopen(f"https://api.geoapify.com/v2/places?categories=tourism.attraction&filter=circle:{countries_info[0]['capitalInfo']['latlng'][0]},{countries_info[0]['capitalInfo']['latlng'][1]},5000&limit=20&apiKey={places_key.read()}")
     places_info = json.load(places)
+    country_data["places"] = places_info
 
-    currencies = []
-    for currency in countries_info[0]['currencies']:
-        currencies.append(currency)
+    currencies = list(countries_info[0]['currencies'].keys())
+    currency_list = ",".join(currencies)
+
     exchange_key = open("keys/key_api3.txt")
-    exchange_rate = urlopen(f"https://api.exchangerateapi.net/v1/latest?base={user_currency}&currencies={currencies[0]+[','+currencies[i] for i in range(1, len(currrencies))]}&apikey={exchange_key.read()}")
+    exchange_rate = urlopen(f"https://api.exchangerateapi.net/v1/latest?base={user_currency}&currencies={currency_list}&apikey={exchange_key.read()}")
     exchange_info = json.load(exchange_rate)
+    country_data["currency"] = exchange_info
 
+    return country_data
 
 
 
 def extract_wikipedia_subsections(title, section_name):
-
 
     sections_req = Request(
             url=f"https://en.wikipedia.org/w/api.php?action=parse&page={title}&prop=sections&format=json",
@@ -88,3 +96,5 @@ def extract_wikipedia_subsections(title, section_name):
         section[heading] = content
 
     return section
+
+pprint.pprint(extract_country_data("Pakistan"))
